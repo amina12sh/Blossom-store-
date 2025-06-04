43647b5680170ab54389cb0e274e01cab35a77ca
amina12sh <aminashukri21@gmail.com>
2025-06-04 11:55:23 +0300

    Add shared constants for security and errors
    Signed-off-by: amina12sh <aminashukri21@gmail.com>

---
+++ constants.js
@@ -0,0 +1,7 @@
+// src/core/constants.js
+
+export const ERROR_MESSAGES = {
+  REQUIRED_FIELDS: 'يرجى تعبئة جميع الحقول',
+  INVALID_EMAIL: 'البريد الإلكتروني غير صالح',
+  SHORT_PASSWORD: 'كلمة المرور قصيرة جداً',
+};
